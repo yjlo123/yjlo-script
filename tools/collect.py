151dac31334861fe27f716d1a128c630436942dc
yjlo123 <liusiwei.yjlo@gmail.com>
2017-10-01 01:30:35 +0800

    Added tool to collect source

--- tools/collect.py
+++ tools/collect.py
@@ -2,15 +2,32 @@
 
 import os
 import json
+import string
 
 def path_to_dict(path):
-	d = {'name': os.path.basename(path)}
+	file_name = os.path.basename(path);
+	title_name = string.replace(os.path.splitext(file_name)[0], '_', ' ').title()
+	d = {'title': title_name}
 	if os.path.isdir(path):
-		d['type'] = "directory"
-		d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
-(path)]
+		d['url'] = file_name
+		d['menu'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir(path) if not x == "Test"]
 	else:
-		d['type'] = "file"
+		d['url'] = file_name
 	return d
 
-print json.dumps(path_to_dict('example'))
+def sorted_nested_menu(menu):
+	if isinstance(menu, list):
+		menu = sorted(menu, key=lambda x: (not 'menu' in x, x['title']))
+		sorted_nested = []
+		for item in menu:
+			if 'menu' in item:
+				item['menu'] = sorted_nested_menu(item['menu'])
+			sorted_nested.append(item)
+		return sorted_nested
+	else:
+		return menu
+
+menu = path_to_dict('example')['menu']
+menu = sorted_nested_menu(menu)
+
+print(json.dumps(menu))
